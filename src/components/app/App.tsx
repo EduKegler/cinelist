b343abee3e1997cf75222a6ade11d03883d85c27
Edu Kegler <eduardokegler1@gmail.com>
2021-05-11 18:19:56 -0300

    feat(Setup): Setup webpack to run react project

---
+++ src/components/app/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './app.scss';
+
+export const App = React.memo(() => {
+    return(<span className="test">Oi</span>)
+});
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '/dist'),
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.html' })],
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    devtool: 'eval-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js)x?$/i,
+                exclude: /node_modules/,
+                use: [{
+                    loader: "babel-loader",
+                    options: {
+                        sourceMaps: true,
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react"
+                        ],
+                    }
+                }]
+            },
+            {
+                test: /\.(s)css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif|jp2|webp|ttf)$/,
+                include: path.join(__dirname, 'src/assets'),
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(ts)x?$/i,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+        disableHostCheck: true,
+        port: 3000,
+        writeToDisk: false,
+    },
+}
